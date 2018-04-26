d74b777fc467bc7948f6b1ec685774dee9f3433d
Emily Jirles <emily.jirles@gmail.com>
2018-04-26 12:47:31 +0000

    Done.

---
+++ code/code.rb
@@ -0,0 +1,11 @@
+# need to submit this lab to move one,
+# but there's nothing to work on. 
+# so this code is only here so i can learn submit 
+
+def double(x)
+  x*2 
+end
+
+def triple(x)
+  x*3
+end
